386056d0ad03dca33e7106be9eca90df1554e8e9
humbejeff2116 <humbejeff2116@gmail.com>
2025-09-16 15:39:05 +0100

    remove src/_core/_micro.services/notification/ folder contents

--- src/_core/_micro.services/notification/database/index.ts
+++
@@ -1,4 +0,0 @@
-import { connectToDatabase, dbTypes } from "./lib/index.js";
-
-// TODO... move DB connection to web index;
-// connectToDatabase(dbTypes.mongoDB);
--- src/_core/_micro.services/notification/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from 'hono';
-
-
-const notificationRouter = new Hono();
-
-notificationRouter.post('/sign-up', notificationController.create);
-notificationRouter.post('/sign-in', notificationController.create);
-
-export default notificationRouter;
