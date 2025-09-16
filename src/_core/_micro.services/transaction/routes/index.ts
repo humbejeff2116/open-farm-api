e5090189d57e4ca49fae8e2eea139f590d0dc5b8
humbejeff2116 <humbejeff2116@gmail.com>
2025-09-16 15:41:31 +0100

    remove src/_core/_micro.services/transaction/ folder contents

--- src/_core/_micro.services/transaction/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from 'hono';
-
-
-const transactionRouter = new Hono();
-
-transactionRouter.post('/sign-up', transactionController.create);
-transactionRouter.post('/sign-in', transactionController.create);
-
-export default transactionRouter;
