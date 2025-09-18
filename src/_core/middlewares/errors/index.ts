8efca66814d9a63de7be410040d71fc0fe946d68
humbejeff2116 <humbejeff2116@gmail.com>
2025-09-18 10:58:26 +0100

    implement operationalErrorHandler, finalErrorHandler functionalities

--- src/_core/middlewares/errors/index.ts
+++ src/_core/middlewares/errors/index.ts
@@ -1,11 +1,42 @@
 import type { NextFunction, Request, Response } from "express";
+import { logger } from "../../utils/logger.utils.js";
+import { errorHandler, type APIError } from "../../utils/error.utils.js";
 
-export function notFound(_req: Request, res: Response) {
-    res.status(404).json({ error: "Not Found" });
+
+export const notFoundRouteHandler = async (req: Request, res: Response) => {
+    logger.warn('route not found');
+    res.status(404).json('route not found');
 }
 
-export function errorHandler(err: any, _req: Request, res: Response, _next: NextFunction) {
-    console.error(err);
-    const status = err?.status || 500;
-    res.status(status).json({ error: err?.message || "Internal Server Error" });
+
+// log and forward error
+export const errorLogerHandler = async (
+    err: Error, 
+    _req: Request, 
+    _res: Response, 
+    next: NextFunction
+) => {
+    logger.error('centralized error handling->>>>', err);
+    next(err);
+}
+
+// handle operational errors
+export const operationalErrorHandler = async (
+    err: APIError, 
+    _req: Request, 
+    res: Response, 
+    next: NextFunction
+) => {
+    if (!errorHandler.isTrustedError(err)) {
+        next(err);
+    }
+    await errorHandler.handleOperationalError(err, res);
+}
+
+export const finalErrorHandler = async (
+    err: Error, 
+    _req: Request, 
+    res: Response
+) => {
+    await errorHandler.handleError(err, res);
 }
